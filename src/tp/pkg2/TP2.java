09c4b9a769236a9bac6d48e7bbf00c820721ca8e
dylan13579 <141684479+dylan13579@users.noreply.github.com>
2024-08-20 19:58:09 -0300

    algunos cambios

--- src/tp/pkg2/TP2.java
+++ src/tp/pkg2/TP2.java
@@ -15,7 +15,18 @@ public class TP2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+      
+        Persona p = new Persona("Marco","Rodriguez",55,180);
+        
+        Reloj r = new Reloj(123,"Samsung");
+        
+        p.setUnReloj(r);
+        
+        p.decirHora(r);
+        
+        r.incrementarHora(2);
+        
+        p.decirHora(r);
         
         
     }
